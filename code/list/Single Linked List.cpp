3874354537dc75e09fedb6ac37fd160ced13b880
prasannabajare <73434401+prasanna4466@users.noreply.github.com>
2021-08-31 14:19:33 +0530

    Update Single Linked List.cpp

--- code/list/Single Linked List.cpp
+++ code/list/Single Linked List.cpp
@@ -1,26 +1,55 @@
-#include<iostream>
+// A simple C++ program for traversal of a linked list
+#include <iostream>
 using namespace std;
 
-struct node{
-	
+class Node {
+public:
 	int data;
-	struct node* next;
-	
+	Node* next;
 };
 
-int main(){
+// This function prints contents of linked list
+// starting from the given node
+void printList(Node* n)
+{
+	while (n != NULL) {
+		cout << n->data << " ";
+		n = n->next;
+	}
+}
+
+// Driver code
+int main()
+{
+	Node* head = NULL;
+	Node* second = NULL;
+	Node* third = NULL;
+
+	// allocate 3 nodes in the heap
+	head = new Node();
+	second = new Node();
+	third = new Node();
 	
-	struct node* head = NULL;
-	struct node* second = NULL;
-	struct node* third = NULL;
-	
-	head->data=1;
-	head->next=second;
-	
-	second->data=2;
-	second->next=third;
-	
-	third->data=3;
-	third->next=NULL;
-	
+	cout<<"enter the element want to insert : ";
+	cin>>head->data; // assign data in first node
+	cout<<"Elements Inserted in list : ";
+	cout<<head->data<<"\n";
+	head->next = second; // Link first node with second
+
+	cout<<"enter the element want to insert : ";
+	cin>>second->data; // assign data to second node
+	cout<<"Elements Inserted in list : ";
+	cout<<second->data<<"\n";
+	second->next = third;
+
+	cout<<"enter the element want to insert : ";
+	cin>>third->data; // assign data to third node
+	cout<<"Elements Inserted in list : ";
+	cout<<third->data<<"\n";
+	third->next = NULL;
+
+	cout<<"The Final list is : ";
+	printList(head);
+
+	return 0;
 }
